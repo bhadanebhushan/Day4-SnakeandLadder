fb79854946019bbaa0ebe3fa188e3bdd9c4902c7
bhadanebhushan <bhadanebhushan97@gmail.com>
2022-03-14 06:29:51 +0530

    Game start zero position

---
+++ src/com/snakeandladder/Player.java
@@ -0,0 +1,10 @@
+package com.snakeandladder;
+
+public class Player {
+
+	public static void main(String[] args) {
+		int position = 0;
+		System.out.println("Initial Position of player starts from " +position);
+	}
+
+}
