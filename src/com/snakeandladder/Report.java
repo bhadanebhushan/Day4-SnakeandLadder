8cc92d981e5f03e606f3de0d2566baa267df824b
bhadanebhushan <bhadanebhushan97@gmail.com>
2022-03-14 07:05:13 +0530

    Report the number of times...

---
+++ src/com/snakeandladder/Report.java
@@ -0,0 +1,45 @@
+package com.snakeandladder;
+import java.util.Random;
+public class Report {
+	static final int Snake = 2;
+	static final int Ladder = 1;
+	static final int Noplay = 0;
+	static int Dicec = 0;
+	public static void main(String[] args) {
+		
+		int position = 0;
+		System.out.println("position: "+position);
+		
+		while (position<100) 
+		{
+			Random ran = new Random();
+			int dice = ran.nextInt(6)+1;
+			System.out.println("dice: "+dice);
+			++Dicec;
+			int optionCheck = ran.nextInt(3);
+			System.out.println("optionCheck: "+optionCheck);
+			
+			
+			if ((optionCheck == Ladder) && (position+dice)<=100) 
+			{
+				System.out.println("ladder");
+				position = position + dice;
+			}
+			else if (optionCheck == Snake) 
+			{
+				System.out.println("Snake");
+				position = position - dice;
+			}
+			else {
+				System.out.println("Noplay");				
+			}
+			if (position<0) 
+			{
+				position =0;
+			}
+			System.out.println("position: "+position+"\ndicecount: "+Dicec);
+			
+		}
+	}
+
+}
